d11e3f499c825398f2ea479d6b27320296dad9b5
Yan Naung Oak <yan@Yans-MacBook-Pro.local>
2022-02-01 13:10:43 +0800

    showcase data added and filter logic working

--- src/pages/[locale]/showcase.js
+++ src/pages/[locale]/showcase.js
@@ -1,98 +1,134 @@
 import LayoutComponent from "@src/components/LayoutComponent";
-import { Container } from "semantic-ui-react";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import {
+    Container,
+    Grid,
+    Dropdown,
+} from "semantic-ui-react";
+import { useTranslation } from "next-i18next";
+
+import showcaseData from '@src/data/showcasedata.json';
+import { showcaseDataParse } from "@src/lib/showcaseHelper";
 
 const ShowCasePage = () => {
+
+    const filters = showcaseDataParse(showcaseData);
+    const defaultIndices = [...Array(showcaseData.length).keys()];
+    const [filteredRows, setFilteredRows] = useState(defaultIndices);
+    const [filteredOptions, setFilteredOptions] = useState({});
+    const { t } = useTranslation("countrylevel");
+    const router = useRouter();
+    const [currentLanguage, setCurrentLanguage] = useState(
+      router.query.locale ? router.query.locale : "en"
+    );
+    const getOptions = (filterCat) => {
+        return Object.keys(filters[filterCat]).map(element => {
+            return {
+                key: element,
+                value: element,
+                text: element,
+            }
+        })
+    };
+    const filterSelectHandler = (cat) => (e,data) => {
+        const selectedOptions = data.value
+        let indicesToSet = [];
+        const newFilteredOptions = ({...filteredOptions, [cat]: selectedOptions});
+        const newFilteredOptionsKeys = Object.keys(newFilteredOptions);
+
+        // Loop through each filter category
+        // for (let k = 0; k < newFilteredOptionsKeys.length; k++) {
+
+        // const currentFilterSelectedOptions = newFilteredOptions[newFilteredOptionsKeys[k]];
+
+        // Loop through each option selected for this filter's categories
+        for (let i = 0; i < selectedOptions.length; i++) {
+            let currentFilterElementIndices = filters[cat][selectedOptions[i]];
+
+            for (let j = 0; j < currentFilterElementIndices.length; j++) {
+                if (!indicesToSet.includes(currentFilterElementIndices[j]))
+                    indicesToSet.push(currentFilterElementIndices[j])
+            }
+        }
+        // }
+        
+
+        setFilteredOptions(newFilteredOptions);
+        setFilteredRows(indicesToSet)
+    }
+
+    
     return (
         <LayoutComponent>
             <div style={{ margin: "1.5rem 0rem" }}>&nbsp;</div>
-            <Container>
-                <h2> Showcase </h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Hendrerit gravida rutrum quisque non tellus orci. Curabitur
-                    gravida arcu ac tortor dignissim. Euismod elementum nisi quis
-                    eleifend quam adipiscing vitae. Vulputate mi sit amet mauris.
-                    Non odio euismod lacinia at quis risus. Porta nibh venenatis
-                    cras sed felis. Consequat interdum varius sit amet mattis
-                    vulputate enim nulla aliquet. Libero volutpat sed cras ornare
-                    arcu dui vivamus arcu felis. Pretium quam vulputate dignissim
-                    suspendisse in est ante in nibh.
-                    <br />
-                    <br /> A diam maecenas sed enim. Ut placerat orci nulla
-                    pellentesque dignissim enim. Morbi tristique senectus et netus
-                    et malesuada fames ac. Posuere sollicitudin aliquam ultrices
-                    sagittis orci a scelerisque purus. Odio tempor orci dapibus
-                    ultrices. Neque sodales ut etiam sit amet nisl. Sit amet
-                    porttitor eget dolor. Consequat ac felis donec et. In cursus
-                    turpis massa tincidunt dui ut. Sodales ut eu sem integer vitae
-                    justo eget. Fermentum et sollicitudin ac orci phasellus. Id
-                    cursus metus aliquam eleifend mi in nulla posuere sollicitudin.
-                    <br />
-                    <br /> A diam maecenas sed enim. Ut placerat orci nulla
-                    pellentesque dignissim enim. Morbi tristique senectus et netus
-                    et malesuada fames ac. Posuere sollicitudin aliquam ultrices
-                    sagittis orci a scelerisque purus. Odio tempor orci dapibus
-                    ultrices. Neque sodales ut etiam sit amet nisl. Sit amet
-                    porttitor eget dolor. Consequat ac felis donec et. In cursus
-                    turpis massa tincidunt dui ut. Sodales ut eu sem integer vitae
-                    justo eget. Fermentum et sollicitudin ac orci phasellus. Id
-                    cursus metus aliquam eleifend mi in nulla posuere sollicitudin.
-                    <br />
-                    <br /> A diam maecenas sed enim. Ut placerat orci nulla
-                    pellentesque dignissim enim. Morbi tristique senectus et netus
-                    et malesuada fames ac. Posuere sollicitudin aliquam ultrices
-                    sagittis orci a scelerisque purus. Odio tempor orci dapibus
-                    ultrices. Neque sodales ut etiam sit amet nisl. Sit amet
-                    porttitor eget dolor. Consequat ac felis donec et. In cursus
-                    turpis massa tincidunt dui ut. Sodales ut eu sem integer vitae
-                    justo eget. Fermentum et sollicitudin ac orci phasellus. Id
-                    cursus metus aliquam eleifend mi in nulla posuere sollicitudin.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Hendrerit gravida rutrum quisque non tellus orci. Curabitur
-                    gravida arcu ac tortor dignissim. Euismod elementum nisi quis
-                    eleifend quam adipiscing vitae. Vulputate mi sit amet mauris.
-                    Non odio euismod lacinia at quis risus. Porta nibh venenatis
-                    cras sed felis. Consequat interdum varius sit amet mattis
-                    vulputate enim nulla aliquet. Libero volutpat sed cras ornare
-                    arcu dui vivamus arcu felis. Pretium quam vulputate dignissim
-                    suspendisse in est ante in nibh.
-                    <br />
-                    <br /> A diam maecenas sed enim. Ut placerat orci nulla
-                    pellentesque dignissim enim. Morbi tristique senectus et netus
-                    et malesuada fames ac. Posuere sollicitudin aliquam ultrices
-                    sagittis orci a scelerisque purus. Odio tempor orci dapibus
-                    ultrices. Neque sodales ut etiam sit amet nisl. Sit amet
-                    porttitor eget dolor. Consequat ac felis donec et. In cursus
-                    turpis massa tincidunt dui ut. Sodales ut eu sem integer vitae
-                    justo eget. Fermentum et sollicitudin ac orci phasellus. Id
-                    cursus metus aliquam eleifend mi in nulla posuere sollicitudin.
-                    <br />
-                    <br /> A diam maecenas sed enim. Ut placerat orci nulla
-                    pellentesque dignissim enim. Morbi tristique senectus et netus
-                    et malesuada fames ac. Posuere sollicitudin aliquam ultrices
-                    sagittis orci a scelerisque purus. Odio tempor orci dapibus
-                    ultrices. Neque sodales ut etiam sit amet nisl. Sit amet
-                    porttitor eget dolor. Consequat ac felis donec et. In cursus
-                    turpis massa tincidunt dui ut. Sodales ut eu sem integer vitae
-                    justo eget. Fermentum et sollicitudin ac orci phasellus. Id
-                    cursus metus aliquam eleifend mi in nulla posuere sollicitudin.
-                    <br />
-                    <br /> A diam maecenas sed enim. Ut placerat orci nulla
-                    pellentesque dignissim enim. Morbi tristique senectus et netus
-                    et malesuada fames ac. Posuere sollicitudin aliquam ultrices
-                    sagittis orci a scelerisque purus. Odio tempor orci dapibus
-                    ultrices. Neque sodales ut etiam sit amet nisl. Sit amet
-                    porttitor eget dolor. Consequat ac felis donec et. In cursus
-                    turpis massa tincidunt dui ut. Sodales ut eu sem integer vitae
-                    justo eget. Fermentum et sollicitudin ac orci phasellus. Id
-                    cursus metus aliquam eleifend mi in nulla posuere sollicitudin.
-                </p>
-                <div style={{ height: '15vh' }}></div>
-            </Container>
+            <Grid>
+                <Grid.Row style={{ margin: "0px", padding: "0px" }}>
+                    <Grid.Column
+                        style={{
+                        background: "#E8EFF2",
+                        color: "black",
+                        paddingLeft: "2.5rem",
+                        paddingTop: "2.5rem",
+                        }}
+                    >
+                        <h4>Filters</h4>
+                    </Grid.Column>
+                </Grid.Row>
+                {
+                    Object.keys(filters).map(filterCat => {
+                        return <Grid.Row style={{ margin: "0px", padding: "0px" }}>
+                            <Grid.Column
+                                style={{
+                                background: "#E8EFF2",
+                                color: "black",
+                                // padding: "2.5rem",
+                                }}
+                            >
+                                <Container>
+                                <h4 style={{ padding: "0.25rem" }}>{t(filterCat)}</h4>
+                                <Dropdown
+                                    placeholder={"Change " + filterCat}
+                                    fluid
+                                    multiple selection
+                                    clearable
+                                    options={getOptions(filterCat)}
+                                    onChange={filterSelectHandler(filterCat)}
+                                    style={{ margin: "0.2rem" }}
+                                />
+                                <br />
+                                
+                                </Container>
+                            </Grid.Column>
+                        </Grid.Row>
+                    })
+                }
+                
+                {
+                    showcaseData
+                        .filter((row,i) => filteredRows.includes(i))
+                        .map(row => {
+                            return <Grid.Row>
+                                <Grid.Column
+                                    style={{
+                                    background: "#E8EFF2",
+                                    color: "black",
+                                    // padding: "2.5rem",
+                                    }}
+                                >
+                                    <h4>{row.title}</h4>
+                                    <p>{row.country}</p>
+                                    <p>{row.language}</p>
+                                    <p>{row.tools}</p>
+                                    <p>{row.themes_keywords}</p>
+                                    <p>{row.algorithms}</p>
+                                </Grid.Column>
+                            </Grid.Row>
+                        })
+                }
+
+            </Grid>
+           
         </LayoutComponent>
     );
 };
