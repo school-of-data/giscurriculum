ecdb8c76dee7a1da72e05515ed7f05cc78560bc8
janakhpon <jnovaxer@gmail.com>
2022-02-01 13:37:33 +0630

    updates: added layout to showcase page

--- src/pages/[locale]/showcase.js
+++ src/pages/[locale]/showcase.js
@@ -1,18 +1,13 @@
 import LayoutComponent from "@src/components/LayoutComponent";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
-import {
-    Container,
-    Grid,
-    Dropdown,
-} from "semantic-ui-react";
+import { Container, Grid, Dropdown, Image, Button } from "semantic-ui-react";
 import { useTranslation } from "next-i18next";
-
-import showcaseData from '@src/data/showcasedata.json';
+import showcaseData from "@src/data/showcasedata.json";
 import { showcaseDataParse } from "@src/lib/showcaseHelper";
+import getLangname from "@src/config/nameCodes";
 
 const ShowCasePage = () => {
-
     const filters = showcaseDataParse(showcaseData);
     const defaultIndices = [...Array(showcaseData.length).keys()];
     const [filteredRows, setFilteredRows] = useState(defaultIndices);
@@ -20,126 +15,178 @@ const ShowCasePage = () => {
     const { t } = useTranslation("countrylevel");
     const router = useRouter();
     const [currentLanguage, setCurrentLanguage] = useState(
-      router.query.locale ? router.query.locale : "en"
+        router.query.locale ? router.query.locale : "en"
     );
     const getOptions = (filterCat) => {
-        return Object.keys(filters[filterCat]).map(element => {
+        return Object.keys(filters[filterCat]).map((element) => {
             return {
                 key: element,
                 value: element,
                 text: element,
-            }
-        })
+            };
+        });
     };
-    const filterSelectHandler = (cat) => (e,data) => {
-        const selectedOptions = data.value
+    const filterSelectHandler = (cat) => (e, data) => {
+        const selectedOptions = data.value;
         let indicesToSet = [...defaultIndices];
-        const newFilteredOptions = ({...filteredOptions, [cat]: selectedOptions});
+        const newFilteredOptions = { ...filteredOptions, [cat]: selectedOptions };
         const newFilteredOptionsKeys = Object.keys(newFilteredOptions);
 
         // Loop through each filter category
         for (let k = 0; k < newFilteredOptionsKeys.length; k++) {
-        const currentCat = newFilteredOptionsKeys[k];
-        const currentFilterSelectedOptions = newFilteredOptions[newFilteredOptionsKeys[k]];
-        let currentCatFilteredIndices = [];
+            const currentCat = newFilteredOptionsKeys[k];
+            const currentFilterSelectedOptions =
+                newFilteredOptions[newFilteredOptionsKeys[k]];
+            let currentCatFilteredIndices = [];
 
             // Loop through each option selected for this filter's categories
 
             if (currentFilterSelectedOptions.length > 0) {
                 for (let i = 0; i < currentFilterSelectedOptions.length; i++) {
-                    let currentFilterElementIndices = filters[currentCat][currentFilterSelectedOptions[i]];
-    
+                    let currentFilterElementIndices =
+                        filters[currentCat][currentFilterSelectedOptions[i]];
+
                     for (let j = 0; j < currentFilterElementIndices.length; j++) {
-                        if (!currentCatFilteredIndices.includes(currentFilterElementIndices[j]))
-                        currentCatFilteredIndices.push(currentFilterElementIndices[j])
+                        if (
+                            !currentCatFilteredIndices.includes(
+                                currentFilterElementIndices[j]
+                            )
+                        )
+                            currentCatFilteredIndices.push(currentFilterElementIndices[j]);
                     }
                 }
             } else {
                 currentCatFilteredIndices = [...defaultIndices];
             }
 
-            indicesToSet = indicesToSet.filter(function(n) {
+            indicesToSet = indicesToSet.filter(function (n) {
                 return currentCatFilteredIndices.indexOf(n) !== -1;
-            })
-            
+            });
         }
-        
 
         setFilteredOptions(newFilteredOptions);
-        setFilteredRows(indicesToSet)
-    }
+        setFilteredRows(indicesToSet);
+    };
 
-    
     return (
         <LayoutComponent>
-            <div style={{ margin: "1.5rem 0rem" }}>&nbsp;</div>
             <Grid>
-                <Grid.Row style={{ margin: "0px", padding: "0px" }}>
+                <Grid.Row style={{ height: "100%", paddingTop: "2rem" }}>
+                    <Grid.Column width={4} style={{ backgroundColor: "#65ABEA" }}>
+                        {Object.keys(filters).map((filterCat) => {
+                            return (
+                                <Grid.Row style={{ margin: "0px", padding: "0px" }}>
+                                    <Grid.Column
+                                        style={{
+                                            color: "black",
+                                            padding: "1rem 0rem 1rem 1.5rem",
+                                        }}
+                                    >
+                                        <Container>
+                                            <h4 style={{ padding: "0.25rem" }}>{t(filterCat)}</h4>
+                                            <Dropdown
+                                                placeholder={"Change " + filterCat}
+                                                fluid
+                                                multiple
+                                                selection
+                                                clearable
+                                                options={getOptions(filterCat)}
+                                                onChange={filterSelectHandler(filterCat)}
+                                                style={{ margin: "0.2rem" }}
+                                            />
+                                            <br />
+                                        </Container>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            );
+                        })}
+                    </Grid.Column>
+                    <Grid.Column width={12}>
+                        {/* {showcaseData
+              .filter((row, i) => filteredRows.includes(i))
+              .map((row) => {
+                return (
+                  <Grid.Row>
                     <Grid.Column
-                        style={{
+                      style={{
                         background: "#E8EFF2",
                         color: "black",
-                        paddingLeft: "2.5rem",
-                        paddingTop: "2.5rem",
-                        }}
+                        // padding: "2.5rem",
+                      }}
                     >
-                        <h4>Filters</h4>
+                      <h4>{row.title}</h4>
+                      <p>{row.country}</p>
+                      <p>{row.language}</p>
+                      <p>{row.tools}</p>
+                      <p>{row.themes_keywords}</p>
+                      <p>{row.algorithms}</p>
+                    </Grid.Column>
+                  </Grid.Row>
+                );
+              })} */}
+
+                        {showcaseData
+                            .filter((row, i) => filteredRows.includes(i))
+                            .map((row) => {
+                                console.log("row", row);
+                                return (
+                                    <Grid style={{
+                                        margin: "0.25rem 1rem 0.25rem 1rem", padding: "0.25rem",
+                                        border: "solid #65ABEA 1px",
+                                        borderRadius: "0.25rem",
+                                        width: "95%",
+                                    }}>
+                                        <Grid.Row
+
+                                        >
+                                            <Grid.Column width={4}>
+                                                <Image src={row.image} alt={row.title}/>
+                                                <h4>
+                                                    Country: <span>{row.country}</span>
+                                                </h4>
+                                                <h4>
+                                                    Language: <span>{getLangname[row.language]}</span>
+                                                </h4>
+                                                <h4>
+                                                    Authors: <span>{row.authors}</span>
+                                                </h4>
+                                            </Grid.Column>
+                                            <Grid.Column width={10}>
+                                                <h3>{row.title}</h3>
+                                                <h4>Project Summary</h4>
+                                                <p>{row.project_summary}</p>
+                                                <h4>Tools</h4>
+                                                <div style={{ display: 'flex', direction: 'row' }}>
+                                                    {row.tools.split(",").map((tool) => (
+                                                        <span style={{ backgroundColor: "#EEEEEE", padding: "0.25rem", marginLeft: "0.25rem", borderRadius: '0.15rem', cursor: 'pointer' }}>{tool}</span>
+                                                    ))}
+                                                </div>
+                                                <h4>Themes and Keywords</h4>
+                                                {row.themes_keywords.split(",").map((tool) => (
+                                                    <span style={{ backgroundColor: "#EEEEEE", padding: "0.25rem", marginLeft: "0.25rem", borderRadius: '0.15rem', cursor: 'pointer' }}>{tool}</span>
+                                                ))}
+                                                <h4>Algorithms</h4>
+                                                {row.algorithms.split(",").map((tool) => (
+                                                    <span style={{ backgroundColor: "#EEEEEE", padding: "0.25rem", marginLeft: "0.25rem", borderRadius: '0.15rem', cursor: 'pointer' }}>{tool}</span>
+                                                ))}
+                                                <h4>Datasets</h4>
+                                                <p>{row.datasets}</p>
+
+
+                                            </Grid.Column>
+                                        </Grid.Row>
+                                        <Grid.Row>
+                                            <Grid.Column width={12}></Grid.Column>
+                                            <Grid.Column width={4}>
+                                                <a href={row.readmore} target="_blank"><Button primary>Read full report</Button></a>
+                                            </Grid.Column>
+                                        </Grid.Row>
+                                    </Grid>
+                                );
+                            })}
                     </Grid.Column>
                 </Grid.Row>
-                {
-                    Object.keys(filters).map(filterCat => {
-                        return <Grid.Row style={{ margin: "0px", padding: "0px" }}>
-                            <Grid.Column
-                                style={{
-                                background: "#E8EFF2",
-                                color: "black",
-                                // padding: "2.5rem",
-                                }}
-                            >
-                                <Container>
-                                <h4 style={{ padding: "0.25rem" }}>{t(filterCat)}</h4>
-                                <Dropdown
-                                    placeholder={"Change " + filterCat}
-                                    fluid
-                                    multiple selection
-                                    clearable
-                                    options={getOptions(filterCat)}
-                                    onChange={filterSelectHandler(filterCat)}
-                                    style={{ margin: "0.2rem" }}
-                                />
-                                <br />
-                                
-                                </Container>
-                            </Grid.Column>
-                        </Grid.Row>
-                    })
-                }
-                
-                {
-                    showcaseData
-                        .filter((row,i) => filteredRows.includes(i))
-                        .map(row => {
-                            return <Grid.Row>
-                                <Grid.Column
-                                    style={{
-                                    background: "#E8EFF2",
-                                    color: "black",
-                                    // padding: "2.5rem",
-                                    }}
-                                >
-                                    <h4>{row.title}</h4>
-                                    <p>{row.country}</p>
-                                    <p>{row.language}</p>
-                                    <p>{row.tools}</p>
-                                    <p>{row.themes_keywords}</p>
-                                    <p>{row.algorithms}</p>
-                                </Grid.Column>
-                            </Grid.Row>
-                        })
-                }
-
             </Grid>
-           
         </LayoutComponent>
     );
 };
